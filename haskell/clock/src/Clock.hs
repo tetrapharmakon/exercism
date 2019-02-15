e6134cadf6d96c02ba75f08b387232757c604941
fouche <tetrapharmakon@gmail.com>
2019-02-15 15:44:53 +0100

    solved clock

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -1,31 +1,36 @@
-{-# LANGUAGE TypeOperators #-}
-
 module Clock (addDelta, fromHourMin, toString) where
 
-newtype Clock = Clock (Int, Int) deriving (Eq,Show)
+data Clock = Clock { hour :: Int , mins :: Int } deriving (Show)
 
-getH :: Clock -> Int
-getH (Clock (a,_)) = a
+instance Eq Clock where
+  Clock h1 m1 == Clock h2 m2 = (rh1 - rh2) `mod` 24 == 0 && (rm1 - rm2) `mod` 60 == 0
+    where 
+      rh1 = hour (reduce (Clock h1 m1))
+      rh2 = hour (reduce (Clock h2 m2))
+      rm1 = mins (reduce (Clock h1 m1))
+      rm2 = mins (reduce (Clock h2 m2))
 
-getM :: Clock -> Int
-getM (Clock (_,b)) = b
+reduce :: Clock -> Clock
+reduce c = Clock rhour rmins
+  where 
+    rmins = mins c `mod` 60
+    rhour = mod (hour c + div (mins c) 60) 24
 
 sumClocks :: Clock -> Clock -> Clock
-sumClocks c d = reduce $ Clock (getH c + getH d, getM c + getM d)
+sumClocks c d = reduce $ Clock (hour rc + hour rd) (mins rc + mins rd)
+  where 
+    rc = reduce c
+    rd = reduce d
 
 hourFormat :: Int -> String
 hourFormat n = if n < 10 then '0' : show n else show n
 
-reduce :: Clock -> Clock
-reduce c = Clock (mod (getH c + quot (getM c) 60) 24, mins)
-  where mins = getM c `mod` 60
-
 fromHourMin :: Int -> Int -> Clock
-fromHourMin hour minu = Clock (hour, minu)
+fromHourMin = Clock
 
 toString :: Clock -> String
-toString clock = hourFormat (getH rclock) ++ ":" ++ hourFormat (getM rclock)
-  where rclock = reduce clock
+toString c = hourFormat (hour rc) ++ ":" ++ hourFormat (mins rc)
+  where rc = reduce c
 
 addDelta :: Int -> Int -> Clock -> Clock
-addDelta hour minu clock = reduce $ sumClocks clock (fromHourMin hour minu)
+addDelta h m c = reduce $ sumClocks c (Clock h m)
