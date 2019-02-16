9339a1c3fa8663672d28eca2562c1fb4c45c6256
fouche <tetrapharmakon@gmail.com>
2019-02-16 12:47:47 +0100

    improved LeapYear.hs

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,8 +1,8 @@
 module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = (x && not y) || (z && y)
+isLeapYear year = x || (y && not z)
   where
-    x = year `rem` 4 == 0
-    y = year `rem` 100 == 0
-    z = quot year 100 `rem` 4 == 0
+    x = year `rem` 400 == 0
+    y = year `rem` 4 == 0
+    z = year `rem` 100 == 0
