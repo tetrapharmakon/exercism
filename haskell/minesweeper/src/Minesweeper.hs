68e8bcfce9b27bace71285f6625635170622158d
fouche <tetrapharmakon@gmail.com>
2019-02-16 16:07:22 +0100

    solved minesweeper

--- haskell/minesweeper/src/Minesweeper.hs
+++ haskell/minesweeper/src/Minesweeper.hs
@@ -5,7 +5,7 @@ type Board = [String]
 -- nameless helpers:
 -- f takes the correct subset of [n-1,n,n+1] in edge cases
 -- g counts how many mines are around an empty cell
--- and return a mine/space otherwise
+-- and returns a mine/space otherwise
 
 f :: Int -> Int -> [Int]
 f n m = filter (\x -> x>=0 && x<=m) [n-1,n,n+1]
@@ -37,7 +37,7 @@ annotate [[]] = [""]
 annotate board 
   | m == 0    = [[ g 0 j board | j <- [0..n]]]
   | n == 0    = map (:[]) (head [[ g i 0 board | i <- [0..m]]])
-  | otherwise = [[ g i j board | i <- [0..m]] | j <- [0..n]]
+  | otherwise = [[ g j i board | i <- [0..m]] | j <- [0..n]]
   where 
     m = length board -1
     n = length (head board) -1
