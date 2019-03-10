602ec8ad0df4362f99a1d6e6b0964819993dbaf9
fouche <tetrapharmakon@gmail.com>
2019-03-10 01:38:32 +0100

    solved word-count

--- haskell/word-count/src/WordCount.hs
+++ haskell/word-count/src/WordCount.hs
@@ -1,6 +1,8 @@
 module WordCount (wordCount) where
 
 import Data.List
+import Data.Char
+import Data.List.Split
 
 pack :: (Eq a) => [a] -> [[a]]
 pack [] = []
@@ -14,12 +16,24 @@ rleCompress y@(x:xs) =
  
 rleReduce :: (Eq a, Ord a) => [(a, Int)] -> [(a, Int)]
 rleReduce [] = []
-rleReduce a = filter appears $ map reducer $ groupBy alike $ sortOn fst a
+rleReduce a = filter appears $ 
+  map reducer $ 
+  groupBy alike $ 
+  sortOn fst a
   where
     appears x = snd x /= 0
     alike x y = fst x == fst y
     reducer w = (fst $ head w, sum $ map snd w)
- 
+
+removeTicks :: String -> String
+removeTicks ('\'':xs) 
+  | last xs == '\'' = init xs
+  | otherwise = xs
+removeTicks as = as
 
 wordCount :: String -> [(String, Int)]
-wordCount xs = rleReduce $ rleCompress $ words xs
+wordCount xs = rleReduce $ rleCompress parole
+  where 
+    parole = filter (/="") $ 
+      concatMap (splitOneOf ", :;.!&@$%^\n\t\r" . removeTicks) $ 
+      words $ map toLower xs
