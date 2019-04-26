941f2738e97c2fa288cb16fcd1a9960bddac3e7b
fouche <tetrapharmakon@gmail.com>
2019-04-26 02:07:31 +0200

    began go cnt

---
+++ haskell/go-counting/src/Counting.hs
@@ -0,0 +1,17 @@
+module Counting (
+    Color(..),
+    territories,
+    territoryFor
+) where
+
+import Data.Set (Set)
+
+data Color = Black | White deriving (Eq, Ord, Show)
+type Coord = (Int, Int)
+type Board = [[Maybe Color]]
+
+territories :: [String] -> [(Set Coord, Maybe Color)]
+territories board = error "You need to implement this function."
+
+territoryFor :: [String] -> Coord -> Maybe (Set Coord, Maybe Color)
+territoryFor board coord = error "You need to implement this function."
