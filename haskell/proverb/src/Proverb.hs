12363560d8c8a079066f12548ca08f9e9f15cb69
fouche <tetrapharmakon@gmail.com>
2019-04-27 20:05:30 +0200

    solved proverb

--- haskell/proverb/src/Proverb.hs
+++ haskell/proverb/src/Proverb.hs
@@ -1,9 +1,18 @@
 module Proverb(recite) where
 
-things = ["nail", "shoe", "horse", "rider", "message", "battle", "kingdom"]
+helper :: [a] -> [[a]]
+helper [x] = [[x]]
+helper [x,y] = [[x], [x,y]]
+helper xs = helper (init xs) ++ [fatTail xs]
+  where
+    fatTail zs = [last (init zs), last zs]
+
+verse :: [String] -> String
+verse [x] = concat ["And all for the want of a ", x, "."]
+verse ss = concat ["For want of a ", head ss, " the ", head (tail ss), " was lost.\n"]
 
 recite :: [String] -> String
 recite [] = ""
-recite [_] = "And all for the want of a nail."
-recite xss = unwords ["For want of a", head rxss, "the", head (tail rxss), "was lost.\n"] ++ recite (tail rxss)
-  where rxss = reverse xss
+recite xss = concatMap verse as ++ verse a
+  where
+    (a:as) = helper xss
