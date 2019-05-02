91e7258b7164a0a09699c7160719a8821a0085f4
fouche <tetrapharmakon@gmail.com>
2019-05-02 22:09:35 +0200

    solved list-ops

--- haskell/list-ops/src/ListOps.hs
+++ haskell/list-ops/src/ListOps.hs
@@ -9,31 +9,28 @@ module ListOps
   , concat
   ) where
 
-import Prelude hiding
-  ( length, reverse, map, filter, foldr, (++), concat )
+import           Prelude hiding (concat, filter, foldr, length, map, reverse,
+                          (++))
 
 foldl' :: (b -> a -> b) -> b -> [a] -> b
--- foldl' = foldr . flip
-foldl' f z = acc
+foldl' f = acc
   where
-    acc [] = z
-    acc (x:xs) = acc xs `f` x
+    acc z []     = z
+    acc w (x:xs) = w `seq` acc (f w x) xs
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
-foldr f z = acc 
-  where
-    acc [] = z
-    acc (x:xs) = x `f` acc xs
+foldr f z []     = z
+foldr f z (x:xs) = f x (foldr f z xs)
 
 length :: [a] -> Int
 length = foldr (\x -> (+) 1) 0
 
 reverse :: [a] -> [a]
-reverse [] = []
+reverse []     = []
 reverse (x:xs) = reverse xs ++ [x]
 
 map :: (a -> b) -> [a] -> [b]
-map _ [] = []
+map _ []     = []
 map f (x:xs) = f x : map f xs
 
 filter :: (a -> Bool) -> [a] -> [a]
