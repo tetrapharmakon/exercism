56d22ad4c87caa834e484894c97887f90479deb2
fouche <tetrapharmakon@gmail.com>
2019-05-02 22:09:50 +0200

    added poker

---
+++ haskell/poker/src/Poker.hs
@@ -0,0 +1,10 @@
+module Poker (bestHands) where
+
+bestHands :: [String] -> Maybe [String]
+bestHands = error "You need to implement this function!"
+
+{-
+* separa le carte con words
+* ordinale (lessicograficamente)
+* scrivi delle funzioni Hand -> Bool
+-}
