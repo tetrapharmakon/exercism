f552de6034e4258d302a32e3ae230ba83d8c690e
fouche <tetrapharmakon@gmail.com>
2019-05-13 14:47:11 +0200

    solved armstrong

--- haskell/armstrong-numbers/src/ArmstrongNumbers.hs
+++ haskell/armstrong-numbers/src/ArmstrongNumbers.hs
@@ -1,4 +1,10 @@
-module ArmstrongNumbers (armstrong) where
+module ArmstrongNumbers where
+
+digs :: Integral x => x -> [x]
+digs 0 = []
+digs x = x `mod` 10 : digs (x `div` 10)
 
 armstrong :: Integral a => a -> Bool
-armstrong = error "You need to implement this function."
+armstrong n = n == sum (zipWith (^) (digs n) (map (const len) [1 ..]))
+  where
+    len = toInteger $ length (digs n)
