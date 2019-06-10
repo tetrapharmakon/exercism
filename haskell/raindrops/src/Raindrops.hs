16ee06bea2fcaf1fc1a5953e7f577032ff0e4da0
fouche <tetrapharmakon@gmail.com>
2019-06-10 18:17:46 +0200

    raindrops

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -1,12 +1,12 @@
 module Raindrops (convert) where
 
-f n
-  | mod n 3 == 0 = "Pling"
-  | mod n 5 == 0 = "Plang"
-  | mod n 7 == 0 = "Plong"
-  | otherwise = ""
+aFP :: Int -> [Int]
+aFP n = [ i | i <- [3,5,7] , n `mod` i == 0]
 
 convert :: Int -> String
-convert n = 
-  if n `mod` 3 == 0 "Pling" 
-    else if ""
+convert n = if null (aFP n) then show n else concatMap f (aFP n)
+    where
+      f 3 = "Pling"
+      f 5 = "Plang"
+      f 7 = "Plong"
+      f _ = ""
