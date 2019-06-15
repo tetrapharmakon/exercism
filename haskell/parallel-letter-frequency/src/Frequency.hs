897d1e05b0251ef53b71d88e24fbcea876c2a10d
fouche <tetrapharmakon@gmail.com>
2019-06-15 18:51:39 +0200

    solved sublist

--- haskell/parallel-letter-frequency/src/Frequency.hs
+++ haskell/parallel-letter-frequency/src/Frequency.hs
@@ -1,7 +1,33 @@
-module Frequency (frequency) where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Data.Map  (Map)
-import Data.Text (Text)
+module Frequency
+  ( frequency
+  ) where
 
-frequency :: Int -> [Text] -> Map Char Int
-frequency nWorkers texts = error "You need to implement this function."
+import Control.Parallel.Strategies (parListChunk, rseq, using)
+import Data.Char
+import Data.List
+import Data.List.Split
+import qualified Data.Map as M (Map, empty, fromList, unionsWith)
+import Data.Text (Text, pack, unpack)
+
+rleCompress :: Eq a => [a] -> [(a, Int)]
+rleCompress t = zip (map head tt) (map Prelude.length tt)
+  where
+    tt = group t
+
+rleReduce :: (Eq a, Ord a) => [(a, Int)] -> [(a, Int)]
+rleReduce [] = []
+rleReduce a = map red $ groupBy alike $ sortOn fst a
+  where
+    alike x y = fst x == fst y
+    red w = (fst $ head w, sum $ map snd w)
+
+frequency :: Int -> [Text] -> M.Map Char Int
+frequency _ [] = M.empty
+frequency nWorkers texts = M.unionsWith (+) freqs
+  where
+    utexts = map toLower $ filter isLetter $ concatMap Data.Text.unpack texts
+    chunks = chunksOf nWorkers utexts
+    strategy = parListChunk chunks rseq
+    freqs = M.fromList (rleReduce $ rleCompress utexts) `using` strategy
--- haskell/parallel-letter-frequency/test/Tests.hs
+++ haskell/parallel-letter-frequency/test/Tests.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Map          (empty, fromList, lookup, singleton)
-import Data.Text         (concat)
-import Test.Hspec        (Spec, describe, it, shouldBe)
+import Data.Map (empty, fromList, lookup, singleton)
+import Data.Text (concat)
+import Prelude hiding (concat, lookup)
+import Test.Hspec (Spec, describe, it, shouldBe)
 import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
-import Prelude    hiding (concat, lookup)
 
 import Frequency (frequency)
 
@@ -13,10 +13,10 @@ main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
 specs = do
-
-    let odeAnDieFreude = concat             -- Poem by Friedrich Schiller.
-          [ "Freude schöner Götterfunken"   -- The corresponding music is
-          , "Tochter aus Elysium,"          -- the European Anthem.
+  let odeAnDieFreude =
+        concat -- Poem by Friedrich Schiller.
+          [ "Freude schöner Götterfunken" -- The corresponding music is
+          , "Tochter aus Elysium," -- the European Anthem.
           , "Wir betreten feuertrunken,"
           , "Himmlische, dein Heiligtum!"
           , "Deine Zauber binden wieder"
@@ -24,7 +24,8 @@ specs = do
           , "Alle Menschen werden Brüder,"
           , "Wo dein sanfter Flügel weilt."
           ]
-        starSpangledBanner = concat         -- American national anthem
+      starSpangledBanner =
+        concat -- American national anthem
           [ "O say can you see by the dawn's early light,"
           , "What so proudly we hailed at the twilight's last gleaming,"
           , "Whose broad stripes and bright stars through the perilous fight,"
@@ -34,7 +35,8 @@ specs = do
           , "O say does that star-spangled banner yet wave,"
           , "O'er the land of the free and the home of the brave?"
           ]
-        wilhelmus = concat                  -- Dutch national anthem
+      wilhelmus =
+        concat -- Dutch national anthem
           [ "Wilhelmus van Nassouwe"
           , "ben ik, van Duitsen bloed,"
           , "den vaderland getrouwe"
@@ -44,39 +46,24 @@ specs = do
           , "den Koning van Hispanje"
           , "heb ik altijd geëerd."
           ]
-        anthems = [odeAnDieFreude, starSpangledBanner, wilhelmus]
-
-    describe "frequency" $ do
-
-      it "no texts mean no letters" $
-        frequency 1 [] `shouldBe` empty
-
-      it "one letter" $
-        frequency 1 ["a"] `shouldBe` singleton 'a' 1
-
-      it "case insensitivity" $
-        frequency 1 ["aA"] `shouldBe` singleton 'a' 2
-
-      it "many empty texts still mean no letters" $
-        frequency 1 (replicate 10000 "  ") `shouldBe` empty
-
-      it "many times the same text gives a predictable result" $
-        frequency 1 (replicate 1000 "abc") `shouldBe` fromList [ ('a', 1000)
-                                                               , ('b', 1000)
-                                                               , ('c', 1000) ]
-
-      it "punctuation doesn't count" $
-        lookup ',' (frequency 1 [odeAnDieFreude]) `shouldBe` Nothing
-
-      it "numbers don't count" $
-        lookup '1' (frequency 1 ["Testing, 1, 2, 3"]) `shouldBe` Nothing
-
-      let testAllAnthems n = do
-            let frequencies = frequency n anthems
-            lookup 'a' frequencies `shouldBe` Just 49
-            lookup 't' frequencies `shouldBe` Just 56
-            lookup 'ü' frequencies `shouldBe` Just  2
-
-      it "all three anthems, together, 1 worker" $ testAllAnthems 1
-
-      it "all three anthems, together, 4 workers" $ testAllAnthems 4
+      anthems = [odeAnDieFreude, starSpangledBanner, wilhelmus]
+  describe "frequency" $ do
+    it "no texts mean no letters" $ frequency 1 [] `shouldBe` empty
+    it "one letter" $ frequency 1 ["a"] `shouldBe` singleton 'a' 1
+    it "case insensitivity" $ frequency 1 ["aA"] `shouldBe` singleton 'a' 2
+    it "many empty texts still mean no letters" $
+      frequency 1 (replicate 10000 "  ") `shouldBe` empty
+    it "many times the same text gives a predictable result" $
+      frequency 1 (replicate 1000 "abc") `shouldBe`
+      fromList [('a', 1000), ('b', 1000), ('c', 1000)]
+    it "punctuation doesn't count" $
+      lookup ',' (frequency 1 [odeAnDieFreude]) `shouldBe` Nothing
+    it "numbers don't count" $
+      lookup '1' (frequency 1 ["Testing, 1, 2, 3"]) `shouldBe` Nothing
+    let testAllAnthems n = do
+          let frequencies = frequency n anthems
+          lookup 'a' frequencies `shouldBe` Just 49
+          lookup 't' frequencies `shouldBe` Just 56
+          lookup 'ü' frequencies `shouldBe` Just 2
+    it "all three anthems, together, 1 worker" $ testAllAnthems 1
+    it "all three anthems, together, 4 workers" $ testAllAnthems 4
