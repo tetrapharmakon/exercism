859b9cb2fd1f543311eeba5b99b9f440b6e56267
fouche <tetrapharmakon@gmail.com>
2019-06-15 18:51:53 +0200

    solved sublist '

--- haskell/sublist/src/Sublist.hs
+++ haskell/sublist/src/Sublist.hs
@@ -1,4 +1,12 @@
-module Sublist (sublist) where
+module Sublist
+  ( sublist
+  ) where
 
-sublist :: [a] -> [a] -> Maybe Ordering
-sublist xs ys = error "You need to implement this function."
+import Data.List
+
+sublist :: Eq a => [a] -> [a] -> Maybe Ordering
+sublist xs ys
+  | xs == ys = Just EQ
+  | xs `isInfixOf` ys = Just LT
+  | ys `isInfixOf` xs = Just GT
+  | otherwise = Nothing
