c25144322f9a2c14ac2492e9d054967127b25499
fouche <tetrapharmakon@gmail.com>
2020-01-16 21:57:15 +0100

    solved binary-searchtree

--- haskell/binary-search-tree/src/BST.hs
+++ haskell/binary-search-tree/src/BST.hs
@@ -1,37 +1,49 @@
 module BST
-    ( BST
-    , bstLeft
-    , bstRight
-    , bstValue
-    , empty
-    , fromList
-    , insert
-    , singleton
-    , toList
-    ) where
+  ( BST
+  , bstLeft
+  , bstRight
+  , bstValue
+  , empty
+  , fromList
+  , insert
+  , singleton
+  , toList
+  ) where
 
-data BST a = Dummy deriving (Eq, Show)
+data BST a
+  = Empty
+  | Branch a (BST a) (BST a)
+  deriving (Eq, Show)
 
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft tree = error "You need to implement this function."
+bstLeft (Branch _ l _) = Just l
+bstLeft _ = Nothing
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight tree = error "You need to implement this function."
+bstRight (Branch _ _ r) = Just r
+bstRight _ = Nothing
 
 bstValue :: BST a -> Maybe a
-bstValue tree = error "You need to implement this function."
+bstValue (Branch a _ _) = Just a
+bstValue Empty = Nothing
 
 empty :: BST a
-empty = error "You need to implement this function."
+empty = Empty
 
 fromList :: Ord a => [a] -> BST a
-fromList xs = error "You need to implement this function."
+fromList [] = Empty
+fromList (x:ys) =
+  Branch x (fromList $ filter (< x) ys) (fromList $ filter (>= x) ys)
 
 insert :: Ord a => a -> BST a -> BST a
-insert x tree = error "You need to implement this function."
+insert x Empty = singleton x
+insert x (Branch y l r)
+  | x > y = Branch y l (insert x r)
+  | otherwise = Branch y (insert x l) r
 
 singleton :: a -> BST a
-singleton x = error "You need to implement this function."
+singleton x = Branch x Empty Empty
 
 toList :: BST a -> [a]
-toList tree = error "You need to implement this function."
+toList Empty = []
+toList (Branch a l r) = toList l ++ (a : toList r)
