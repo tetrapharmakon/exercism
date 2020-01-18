c61a1246f3c46edf207fc2b7645a058f902d330f
fouche <tetrapharmakon@gmail.com>
2020-01-18 11:32:34 +0100

    solved allergies

--- haskell/allergies/src/Allergies.hs
+++ haskell/allergies/src/Allergies.hs
@@ -15,11 +15,25 @@ data Allergen
   | Cats
   deriving (Eq, Show)
 
-values :: [Int]
-values = [1, 2, 4, 8, 16, 32, 64, 128]
+aller :: [Allergen]
+aller =
+  [Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats]
+
+{- 
+  1. take the integer
+  2. generate its base 2 ciphers
+  3. zip with list of allergens IN ORDER
+  4. filter all (1, Allergen) and discard all (0, Allergen) 
+-}
+toBase2 :: Int -> [Int]
+toBase2 0 = []
+toBase2 1 = [1]
+toBase2 n = toBase2 (div n 2) ++ [mod n 2]
 
 allergies :: Int -> [Allergen]
-allergies score = error "You need to implement this function."
+allergies score =
+  let ns = reverse (toBase2 score)
+   in map fst $ filter (\x -> snd x == 1) $ zip aller ns
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo allergen score = error "You need to implement this function."
+isAllergicTo allergen score = allergen `elem` allergies score
--- haskell/alphametics/src/Alphametics.hs
+++ haskell/alphametics/src/Alphametics.hs
@@ -1,4 +1,17 @@
-module Alphametics (solve) where
+module Alphametics
+  ( solve
+  ) where
+
+import Data.List
+
+alph :: String
+alph = ['A' .. 'Z']
+
+addends :: String -> [String]
+addends = filter (/= "+") . takeWhile (/= "==") . words
+
+result :: String -> String
+result = last . dropWhile (/= "==") . words
 
 solve :: String -> Maybe [(Char, Int)]
 solve puzzle = error "You need to implement this function."
